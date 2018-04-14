90b1b66368a1717ab351d9c18b62715c2600bdb7
amirkhanian99 <35461426+amirkhanian99@users.noreply.github.com>
2018-04-15 00:50:36 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace ParallelLoops
