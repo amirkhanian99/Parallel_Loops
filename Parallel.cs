2eb69d008342d80050cadfa0964740ad90c15daf
amirkhanian99 <35461426+amirkhanian99@users.noreply.github.com>
2018-04-15 00:45:57 +0400

    Add files via upload

---
+++ Parallel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelLoops
+{
+    public static class Parallel
+    {
+        /// <summary>
+        /// Does for loop in a parallel way.
+        /// </summary>
+        /// <param name="fromInclusive"> Inclusive start index. </param>
+        /// <param name="toExclusive"> Exclusive end index. </param>
+        /// <param name="body"> Fuction that is invoked once per iteration. </param>
+        public static void ParallelFor(int fromInclusive, int toExclusive, Action<int> body)
+        {
+           
+            List<Task> taskList = new List<Task>();
+
+            if (body == null)
+            {
+                throw new ArgumentNullException("Body must be non-null to be invoked.");
+            }
+
+            TaskFactory taskFactory = new TaskFactory();
+            for (var counter = fromInclusive; counter < toExclusive; counter++)
+            {
+                var counterCopy = counter;
+                var task = taskFactory.StartNew(() => body(counterCopy));
+                taskList.Add(task);
+            }
+
+            Task.WaitAll(taskList.ToArray());
+        }
+
+        /// <summary>
+        /// Does foreach loop in a parallel way.
+        /// </summary>
+        /// <typeparam name="TSource"> Type of source. </typeparam>
+        /// <param name="source"> Source to be iterated. </param>
+        /// <param name="body"> Function which will be invoked once per iteration. </param>
+        public static void ParallelForEach<TSource>(IEnumerable<TSource> source, Action<TSource> body)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source is null");
+            }
+
+            if (body == null)
+            {
+                throw new ArgumentNullException("Function must be non-null to be invoked.");
+            }
+
+            List<Task> taskList = new List<Task>();
+            TaskFactory taskFactory = new TaskFactory();
+            foreach (var currentElement in source)
+            {
+                var task = taskFactory.StartNew(() => body(currentElement));
+                taskList.Add(task);
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+        }
+
+        /// <summary>
+        /// Does foreach loop with parallel options in a parallel way.
+        /// </summary>
+        /// <typeparam name="TSource"> Type of source. </typeparam>
+        /// <param name="source"> Source to be iterated. </param>
+        /// <param name="parallelOptions"> Parallel options. </param>
+        /// <param name="body"> Function which will be invoked once for iteration.</param>
+        public static void ParallelForEachWithOptions<TSource>(IEnumerable<TSource> source, ParallelOptions parallelOptions,
+            Action<TSource> body)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source is null");
+            }
+
+            if (body == null)
+            {
+                throw new ArgumentNullException("Function must be non-null to be invoked");
+            }
+
+            TaskFactory taskFactory = new TaskFactory(parallelOptions.CancellationToken, TaskCreationOptions.None,
+                TaskContinuationOptions.None, parallelOptions.TaskScheduler);
+            List<Task> taskList = new List<Task>();
+            foreach (var currentElement in source)
+            {
+                var task = taskFactory.StartNew(() => body(currentElement));
+                taskList.Add(task);
+            }
+
+            Task.WaitAll(taskList.ToArray());
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelLoops
+{
+    class Program
+    { 
+        public static void Main()
+        {
+            //ParallelFor test
+            Console.WriteLine("Parallel for test!");
+            Parallel.ParallelFor(0,100,Console.WriteLine);
+            Console.ReadLine();
+
+            //ParallelForEach test
+            Console.WriteLine("Parallel ForEach test!");
+            List<int> list = new List<int>();
+            for (var counter = 0; counter < 100; counter++)
+            {
+                list.Add(counter);
+            }
+            Parallel.ParallelForEach(list,Console.WriteLine);
+            Console.ReadLine();
+
+            //Parallel For Each with options test
+            Console.WriteLine("Parallel ForEach with options Test!");
+            Parallel.ParallelForEachWithOptions(list, new ParallelOptions() {MaxDegreeOfParallelism = 4}, Console.WriteLine);
+            Console.ReadLine();
+        }
+
+       
+    }
+}
